1e16092a783f6ed0325fcf27707c3f6425619938
xiaweizi <1012126908@qq.com>
2018-03-22 21:14:36 +0800

    [dev] 测试用例添加

--- app/src/main/java/com/xiaweizi/downloadloadingview/MainActivity.java
+++ app/src/main/java/com/xiaweizi/downloadloadingview/MainActivity.java
@@ -1,13 +1,64 @@
 package com.xiaweizi.downloadloadingview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+
+import com.xiaweizi.lib.DownloadLoadingView;
+
+import java.lang.ref.WeakReference;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DownloadLoadingView mLoadingView;
+    private DownloadLoadingView mLoadingView1;
+    private DownloadLoadingView mLoadingView2;
+    private DownloadLoadingView mLoadingView3;
+    private MyHandler mHandler;
+
+    private static final int MSG_DOWNLOAD = 101;
+    private int mProgress = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLoadingView = findViewById(R.id.loading_view);
+        mLoadingView1 = findViewById(R.id.loading_view1);
+        mLoadingView2 = findViewById(R.id.loading_view2);
+        mLoadingView3 = findViewById(R.id.loading_view3);
+        mHandler = new MyHandler(this);
+        mHandler.sendEmptyMessage(MSG_DOWNLOAD);
+    }
+
+    static class MyHandler extends Handler {
+        WeakReference<MainActivity> mActivity;
+
+        MyHandler(MainActivity activity) {
+            mActivity = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            MainActivity theActivity = mActivity.get();
+            if (theActivity == null || theActivity.isFinishing()) {
+                return;
+            }
+            // 消息处理
+            switch (msg.what) {
+                case MSG_DOWNLOAD:
+                    theActivity.mProgress += 1;
+                    theActivity.mLoadingView.setProgress(theActivity.mProgress % 100);
+                    theActivity.mLoadingView1.setProgress(theActivity.mProgress % 100);
+                    theActivity.mLoadingView2.setProgress(theActivity.mProgress % 100);
+                    theActivity.mLoadingView3.setProgress(theActivity.mProgress % 100);
+                    theActivity.mHandler.sendEmptyMessageDelayed(MSG_DOWNLOAD, 20);
+                    break;
+                default:
+                    break;
+            }
+
+        }
     }
 }
--- lib/src/main/java/com/xiaweizi/lib/DownloadLoadingView.java
+++ lib/src/main/java/com/xiaweizi/lib/DownloadLoadingView.java
@@ -8,8 +8,6 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -27,56 +25,68 @@ import android.view.View;
 public class DownloadLoadingView extends View {
 
     private static final String TAG = "CustomView";
+    /** 默认的环的宽度 */
     private static final float STROKE_WIDTH_DEFAULT = 5;
+    /** 默认的半径 */
     private static final float RADIUS_DEFAULT = 50;
+    /** 默认最大的进度 */
     private static final int MAX_PROGRESS_DEFAULT = 100;
+    /** 圆角的半径默认值 */
+    private static final int ROUND_RADIUS_DEFAULT = 12;
 
     private Paint paint ;
     private float mStrokeWidth;
     private float mRadius;
-    private int mProgress = 50;
-    private int mMaxProgress = MAX_PROGRESS_DEFAULT;
-
+    private float mRoundRadius;
+    private int mProgress;
+    private int mMaxProgress;
 
     public DownloadLoadingView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        TypedArray typedArray = context.obtainStyledAttributes(attrs,
-                R.styleable.CustomView);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CustomView);
         mRadius = typedArray.getDimension(R.styleable.CustomView_radius, RADIUS_DEFAULT);
         mStrokeWidth = typedArray.getDimension(R.styleable.CustomView_strokeWidth, STROKE_WIDTH_DEFAULT);
         mMaxProgress = typedArray.getInteger(R.styleable.CustomView_maxProgress, MAX_PROGRESS_DEFAULT);
+        mRoundRadius = typedArray.getDimension(R.styleable.CustomView_roundRadius, ROUND_RADIUS_DEFAULT);
         Log.i(TAG, "radius:" + mRadius);
         typedArray.recycle();
-
         setLayerType(View.LAYER_TYPE_SOFTWARE, null);//关闭硬件加速
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         // 绘制背景
-        paint.setColor(Color.argb(100, 0, 0, 0));
+        paint.setColor(Color.argb(180, 0, 0, 0));
         paint.setStyle(Paint.Style.FILL);
-        canvas.drawRoundRect(0, 0, getWidth(), getHeight(), 12, 12, paint);
+        RectF round = new RectF(0, 0, getWidth(), getHeight());
+        canvas.drawRoundRect(round, mRoundRadius, mRoundRadius, paint);
 
-        // 绘制进度
+        // 绘制圆环
         paint.setColor(Color.RED);
         paint.setStrokeWidth(mStrokeWidth);
+        // 采用 clear 的方式
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
         paint.setStyle(Paint.Style.STROKE);
         canvas.drawCircle(getWidth()/2,getHeight()/2, mRadius, paint);
+        // 绘制内圆
         paint.setStyle(Paint.Style.FILL);
         float sweepAngle = 360 * mProgress / mMaxProgress;
         RectF rectF = new RectF(getWidth()/2-mRadius, getHeight()/2-mRadius, getWidth()/2+mRadius, getHeight()/2 + mRadius );
         canvas.drawArc(rectF , -90, sweepAngle, true, paint);
+        // 记得设置为 null 不然会没有效果
         paint.setXfermode(null);
     }
 
+    /**
+     * @param progress 设置进度
+     */
     public void setProgress(int progress) {
-        this.mProgress = progress;
-        postInvalidate();
+        if (progress >=0 && progress <= mMaxProgress) {
+            this.mProgress = progress;
+            postInvalidate();
+        }
     }
 
     public int getProgress() {
